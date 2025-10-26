92cb50f24c3740b537818ef7df7fd51041ff6f11
Kemal Dermawan <kemaldermawan2707@gmail.com>
2025-10-26 15:55:21 +0700

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+var dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(function(dropdown) {
+    dropdown.addEventListener('mouseover', function() {
+        this.querySelector('.dropdown-content').style.display = 'block';
+    });
+
+    dropdown.addEventListener('mouseout', function() {
+        this.querySelector('.dropdown-content').style.display = 'none';
+    });
+});
